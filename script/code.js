d7a234027ed2321f00d66435d9a79e1e4c4adf7e
Erin-Caitlin <erinwilliamscat@gmail.com>
2024-05-29 20:54:28 +0200

    update

--- script/code.js
+++ script/code.js
@@ -1,18 +1,20 @@
 let data = ['Erin', 4, 6, 9, 11, 'Williams']
 
 // Question 1 
-console.log(`Age:${data.concat(19)}`);
+console.log(`Q1:${data.concat(19)}`);
 // Question 2
+console.log(`Q2: ${data.concat(19).join(', ')}`);
 // Question 3
-// Question 4
-// Question 5
-// Qustion 6 
 let numbers = data.filter(data => typeof data === 'number')
-console.log(numbers);
+console.log(`Q3: ${numbers}`);
+// Question 4
+let value = data.find(data => typeof data === 'string');
+console.log(`Q4: ${value}`);
+// Question 5
+console.log(`Q5: ${data.indexOf('Erin')}`);
+// Qustion 6 
+
 // Question 7
+// Concat 
 // Question 8
-console.log(`Index 1-3: ${data.slice(1, 3)}`);
-
-
-
-
+console.log(`Q8: ${data.slice(1, 4)}`);
