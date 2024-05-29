ef416a5dc4195d7d27c2cfa2eaeb34d88f9d2196
Erin-Caitlin <erinwilliamscat@gmail.com>
2024-05-29 20:26:42 +0200

    function

---
+++ script/code.js
@@ -0,0 +1,18 @@
+let data = ['Erin', 4, 6, 9, 11, 'Williams']
+
+// Question 1 
+console.log(`Age:${data.concat(19)}`);
+// Question 2
+// Question 3
+// Question 4
+// Question 5
+// Qustion 6 
+let numbers = data.filter(data => typeof data === 'number')
+console.log(numbers);
+// Question 7
+// Question 8
+console.log(`Index 1-3: ${data.slice(1, 3)}`);
+
+
+
+
